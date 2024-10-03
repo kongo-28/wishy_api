e493280f98a6ffc70b949f15da589ab6b64377be
kongo-28 <ryota.tennis.ku@gmail.com>
2024-10-03 10:39:23 +0900

    set_userを定義し、indexとactionのbefore_actionに設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: %i[ index action]
+  before_action :authenticate_user!, only: %i[ index action ]
+  before_action :set_user, only: %i[ index action ]
   before_action :set_wishes, only: %i[ index action ]
 
   # GET /users/:id
   def index
-    @user = current_user
     render json: {user: @user,wishes: @wishes}
   end
 
   # GET /users/action
   def action
-    @user = current_user
+
     wish_descriptions = @wishes.map { 
       |wish| "Wish: #{wish["title"]}, likes: #{wish[:likes][0].count}" 
     }.join("\n")
@@ -37,6 +37,10 @@ class UsersController < ApplicationController
       )
     end
     @wishes = wishes_with_user_likes
+  end
+
+  def set_user
+    @user = current_user
   end
 
 
