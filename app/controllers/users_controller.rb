34fc569261f95cf24c1419622df1286aec7407c5
kongo-28 <ryota.tennis.ku@gmail.com>
2024-10-03 19:41:35 +0900

    usersコントローラのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,40 +12,36 @@ class UsersController < ApplicationController
 
   # GET /users/action
   def action
-    chat_gpt_service = ChatGptService.new
-    prompt= "500字以内で返して。話し言葉で返してください。以下のリストを参考にして次の日曜日のアクションプランを考えてください。wishは今後やってみたいことです。likesは熱意を表します。大きな数字ほどより強い気持ちです。「like」「wish」などの単語は適切に言い換えてください。"
-    full_prompt = "#{prompt}\n\nWishリスト:\n#{@wish_descriptions}"
-    @action_plan = chat_gpt_service.chat(full_prompt)
-
-    render json: {action_plan:@action_plan}
+    render json: { action_plan: generate_action_plan }
   end
 
-# GET /users/candidate
+  # GET /users/candidate
   def candidate
-    chat_gpt_service = ChatGptService.new
-    prompt= "500字以内で返して。話し言葉で返してください。以下のリストを参考にして私の好みを推測してください。その後、リストにあるもの以外で新たなwishを3つ提案してください。新たな提案は、【】で囲ってください。wishは今後やってみたいことです。likesは熱意を表します。大きな数字ほどより強い気持ちです。「like」「wish」などの単語は適切に言い換えてください。"
-    full_prompt = "#{prompt}\n\nWishリスト:\n#{@wish_descriptions}"
-    @candidate = chat_gpt_service.chat(full_prompt)
-
-    render json: {candidate:@candidate}
-    # render json: {full_prompt:full_prompt,candidate:@candidate}
+    render json: { candidate: generate_candidate }
   end
 
 
   private
 
+  def set_user
+    @user = current_user
+  end
+
+  def set_test_user
+    @user = User.find(2)
+  end
+
   # ログインしているユーザーが1以上いいねしたwishとそれにひもづくlikeの情報のリスト
   def set_wishes
     @wishes = Wish.joins(:likes)
-    .where(likes: { user_id: @user.id })
-    .where('likes.count >= ?', 1)
-    .order("updated_at DESC")
-    wishes_with_user_likes = @wishes.map do |wish|
-      wish.as_json.merge(
-      likes: wish.likes.select { |like| like.user_id == @user.id }
-      )
-    end
-    @wishes = wishes_with_user_likes
+                  .where(likes: { user_id: @user.id })
+                  .where('likes.count >= ?', 1)
+                  .order(updated_at: :desc)
+                  .map do |wish|
+                    wish.as_json.merge(
+                      likes: wish.likes.select { |like| like.user_id == @user.id }
+                    )
+                  end
   end
 
   def wishes_to_descriptions
@@ -54,12 +50,18 @@ class UsersController < ApplicationController
     }.join("\n")
   end
 
-  def set_user
-    @user = current_user
+  def generate_action_plan
+    chat_gpt_service = ChatGptService.new
+    prompt = "500字以内で返して。話し言葉で返してください。以下のリストを参考にして次の日曜日のアクションプランを考えてください。wishは今後やってみたいことです。likesは熱意を表します。大きな数字ほどより強い気持ちです。「like」「wish」などの単語は適切に言い換えてください。"
+    full_prompt = "#{prompt}\n\nWishリスト:\n#{@wish_descriptions}"
+    chat_gpt_service.chat(full_prompt)
   end
 
-  def set_test_user
-    @user = User.find(2)
+  def generate_candidate
+    chat_gpt_service = ChatGptService.new
+    prompt = "500字以内で返して。話し言葉で返してください。以下のリストを参考にして私の好みを推測してください。その後、リストにあるもの以外で新たなwishを3つ提案してください。新たな提案は、【】で囲ってください。wishは今後やってみたいことです。likesは熱意を表します。大きな数字ほどより強い気持ちです。「like」「wish」などの単語は適切に言い換えてください。"
+    full_prompt = "#{prompt}\n\nWishリスト:\n#{@wish_descriptions}"
+    chat_gpt_service.chat(full_prompt)
   end
 
 end
